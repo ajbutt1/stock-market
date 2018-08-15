8e97abe48f2d6b0bd785ada3bae52c253445e528
Josué Estévez Fernández <jestfer6@gmail.com>
2018-08-15 23:05:17 +0100

    another isolated tests to check toggle stock favorite

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,4 +10,14 @@ fdescribe('AppComponent', () => {
     expect(appComponent.stockObj).toEqual(
       new Stock('Test Stock Company - 1', 'TSC', 85, 80));
   });
+
+  it('should have toggle stock favorite', () => {
+    const appComponent = new AppComponent();
+    appComponent.ngOnInit();
+    expect(appComponent.stockObj.favorite).toEqual(false);
+    appComponent.onToggleFavorite(new Stock('Test', 'TEST', 54, 55));
+    expect(appComponent.stockObj.favorite).toBeTruthy();
+    appComponent.onToggleFavorite(new Stock('Test', 'TEST', 54, 55));
+    expect(appComponent.stockObj.favorite).toBeFalsy();
+  });
 });
